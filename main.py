39ad74a8cb921e4beacea50877a39bd6924f2ce4
clarusiubar <clarusiubar@gmail.com>
2023-08-27 21:35:11 +0900

    Add test edit te1 and tix btn1 handler function

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import sys
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout,
-                             QMessageBox)
+                             QMessageBox, QPlainTextEdit)
 from PyQt5.QtGui import QIcon
 
 class Calculator(QWidget):
@@ -13,10 +13,14 @@ class Calculator(QWidget):
 
     def initUI(self):
 
+        self.te1 = QPlainTextEdit()
+        self.te1.setReadOnly(True)
+
         self.btn1 = QPushButton('Message', self)
         self.btn1.clicked.connect(self.activateMessage)
 
         vbox = QVBoxLayout()
+        vbox.addWidget(self.te1)
         vbox.addStretch(1)
         vbox.addWidget(self.btn1)
         vbox.addStretch(1)
@@ -28,8 +32,9 @@ class Calculator(QWidget):
         self.resize(256,256)
         self.show()
 
-        def activateMessage(self):
-            QMessageBox.information(self, "information", "Button clicked!")
+    def activateMessage(self):
+        # QMessageBox.information(self, "information", "Button clicked!")
+        self.te1.appendPlainText("Button clicked!")
 
 if __name__=='__main__':
     app = QApplication(sys.argv)
